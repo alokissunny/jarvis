68ac4888d3e3fcf77b13cc0109ec592e917cc502
Alok Srivastava <alsriva@microsoft.com>
2023-02-19 22:20:35 +0530

    test commit

--- talking_avatar_backend/helpers/tts.js
+++ talking_avatar_backend/helpers/tts.js
@@ -5,7 +5,7 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: "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" //process.env.OPENAI_API_KEY,
+  apiKey: "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" //process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
@@ -41,20 +41,9 @@ async function generateText(prompt) {
 }
 function parseResponse(response) {
     console.log(`Generated text: ${response.data.choices[0].text}`);
-    // console.log(`Tokens: ${response.choices[0].tokens}`);
-    // console.log(`Completion: ${response.choices[0].completions}`);
-    // console.log(`Probabilities: ${response.choices[0].probabilities}`);
-    // console.log(`Log probabilities: ${response.choices[0].log_probs}`);
 }
-
-// let SSML = `<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xml:lang="hi-IN">
-// <voice name="hi-IN-MadhurNeural">
-//   <mstts:viseme type="FacialExpression"/>
-//   __TEXT__
-// </voice>
-// </speak>`;
-let SSML = `<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xml:lang="en-US">
-<voice name="en-US-JennyNeural">
+let SSML = `<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xml:lang="hi-IN">
+<voice name="hi-IN-SwaraNeural">
   <mstts:viseme type="FacialExpression"/>
   __TEXT__
 </voice>
@@ -96,26 +85,13 @@ const textToSpeech = async (text, voice)=> {
         let timeStep = 1/60;
         let timeStamp = 0;
          const synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
-    //     var audioFile = "YourAudioFile.mp3";
-    // // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
-    // const speechConfig = sdk.SpeechConfig.fromSubscription(process.env.AZURE_KEY, process.env.AZURE_REGION);
-    // speechConfig.speechSynthesisOutputFormat = 5;
-    // const audioConfig = sdk.AudioConfig.fromAudioFileOutput(audioFile);
-
-    // // The language of the voice that speaks.
-    // speechConfig.speechSynthesisVoiceName = "hi-IN-MadhurNeural"; 
-
-    // // Create the speech synthesizer.
-    // var synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
-
+   
         // Subscribes to viseme received event
         synthesizer.visemeReceived = function (s, e) {
 
             // `Animation` is an xml string for SVG or a json string for blend shapes
             var animation = JSON.parse(e.animation);
-            console.log('visemeReceived');
             _.each(animation.BlendShapes, blendArray => {
-                console.log('visemeReceived  ....');
                 let blend = {};
                 _.each(blendShapeNames, (shapeName, i) => {
                     blend[shapeName] = blendArray[i];
